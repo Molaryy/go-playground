12fd0edb9ec25db78ff59d820d696861cd7152a9
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-08-03 00:45:20 +0200

    feat: added quiz from csv file

---
+++ gophercises/quiz-game/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func startQuiz(data []byte) {
+	arrayFile := strings.Split(string(data), "\n")
+	nbCorrectAnswers := 0
+	nbIncorrectAnswers := 0
+	for i := 0; i < len(arrayFile); i++ {
+		cmpResult := strings.Split(arrayFile[i], ",")
+		if len(cmpResult) <= 1 {
+			continue
+		}
+		userInput := ""
+		fmt.Print(cmpResult[0], ": ")
+		fmt.Scan(&userInput)
+		if userInput == cmpResult[1] {
+			nbCorrectAnswers += 1
+		} else {
+			nbIncorrectAnswers += 1
+		}
+	}
+	fmt.Println("Correct answers = ", nbCorrectAnswers)
+	fmt.Println("Incorrect answers = ", nbIncorrectAnswers)
+}
+
+func main() {
+	var argLen int = len(os.Args)
+	useHelp := flag.Bool("fp", false, "Please select a file")
+	flag.Parse()
+	data, err := os.ReadFile("problems.csv")
+	check(err)
+
+	if *useHelp && argLen > 2 {
+		data, err = os.ReadFile(os.Args[2])
+		check(err)
+		startQuiz(data)
+		return
+	}
+	startQuiz(data)
+}
