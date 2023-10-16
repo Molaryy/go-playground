12ece32d6343c02e9065657becb20a1588301842
mohammed <mohammed@heyliot.com>
2023-10-16 14:00:21 +0200

    feat: started bank api

---
+++ go-postgres/api.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+
+type APIServer struct {
+	listenAddr string
+}
+
+func NewApiServer(listenAddr string) *APIServer {
+	return &APIServer{
+		listenAddr: listenAddr,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", s.handleAccount)
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
---
+++ go-postgres/go.mod
@@ -0,0 +1,5 @@
+module github.com/Molaryy/go-playground/go-postgres
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
